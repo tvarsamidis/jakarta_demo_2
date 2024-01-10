ac9ecb2e89b21ed7fdcc50cdca3c6410a7ac4ce1
Tom G. Varsamidis 27693 <tom@27693.super>
2024-01-10 11:50:42 +0200

    Add logger and logger demo

--- src/main/java/gr/codehub/jak20242/Person.java
+++ src/main/java/gr/codehub/jak20242/Person.java
@@ -1,4 +1,16 @@
 package gr.codehub.jak20242;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Person {
+
+    private static final Logger logger = LoggerFactory.getLogger(Person.class);
+
+    public static void main(String[] args) {
+        logger.info("Program started");
+        int i = 4 * 23456;
+        System.out.println(i);
+        logger.warn("Program {} finished", i);
+    }
 }
